9c55bca1847a335871e62000a128ffd12b92a774
rho song <13165182+rhoboat@users.noreply.github.com>
2026-05-31 10:52:02 -0700

    ⚛ use atomic counters

---
+++ 15-atomic-counters/main.go
@@ -0,0 +1,28 @@
+// Demonstrates that atomic counters provide a way to
+// update the same counter with many concurrent goroutines
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+const routines = 50
+const increments = 1000
+
+func main() {
+	var ops atomic.Uint64
+	var wg sync.WaitGroup
+
+	for range routines { // start 50 goroutines
+		wg.Go(func() {
+			for range increments { // each increments the ops counter 1000x
+				ops.Add(1)
+			}
+		})
+	}
+
+	wg.Wait()
+	fmt.Println(ops.Load())
+}
