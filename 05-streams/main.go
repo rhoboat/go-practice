b731a641243cee8a868fa30be33e805a1b109c1c
rho song <13165182+rhoboat@users.noreply.github.com>
2026-06-02 09:42:03 -0700

    move streams

--- 05-streams/main.go
+++ 05-streams/main.go
@@ -1,7 +1,80 @@
 package main
 
 import "fmt"
+import "context"
 
 func main() {
-	fmt.Println("hi")
+	in := []int{2, 4, 5, 6, 8, 10}
+	fmt.Println(in)
+	ctx := context.Background()
+	out := Collect(ctx, Transform(ctx, transformer, Filter(ctx, filter, Stream(ctx, in))))
+	fmt.Println(out)
+}
+
+func transformer(i int) int {
+	return i*i*i
+}
+
+func filter(i int) bool {
+	return i%5!=0
+}
+
+func Stream[T any](ctx context.Context, in []T) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		for _, element := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- element:
+			}
+		}
+	}()
+	return out
+}
+
+func Filter[T any](ctx context.Context, predicate func(T) bool, in <-chan T) <-chan T {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		for element := range in {
+			if predicate(element) {
+				select {
+				case <-ctx.Done():
+					return
+				case out <- element:
+				}
+			}
+		}
+	}()
+	return out
+}
+
+func Transform[I any, O any](ctx context.Context, transformer func(I) O, in <-chan I) <-chan O {
+	out := make(chan O)
+	go func() {
+		defer close(out)
+		for element := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- transformer(element):
+			}
+		}
+	}()
+	return out
+}
+
+func Collect[T any](ctx context.Context, in <-chan T) []T {
+	out := make([]T, 0) // TODO: why zero?
+	for element := range in {
+		select {
+		case <-ctx.Done():
+			return out
+		default:
+			out = append(out, element)
+		}
+	}
+	return out
 }
